ebd9905308b84667e6d3719c0dd968a44a0a8510
Sebastian Octavian SAS <sebastianoctavian.sas@yahoo.com>
2017-05-22 03:17:38 +0300

    Add files via upload

---
+++ PropertySales/Data/Services/IPropertyInfos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Services
+{
+    public interface IPropertyInfos
+    {
+        List<PropertyDetails> getAll();
+
+        PropertyDetails get(Guid id);
+
+        void ChangeState(Guid id);
+
+        void Add(PropertyDetails property);
+    }
+}
